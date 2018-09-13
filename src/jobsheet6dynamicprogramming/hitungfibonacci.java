7048d5770fd9565fec7469d7ef242cf361bf5b26
PC <PC@LAPTOP-HTFI5LEL>
2018-09-13 10:16:05 +0700

    Initial Commit

--- src/jobsheet6dynamicprogramming/hitungfibonacci.java
+++ src/jobsheet6dynamicprogramming/hitungfibonacci.java
@@ -13,46 +13,15 @@ import java.util.Scanner;
  * @author PC
  */
 public class hitungfibonacci {
-     public static void main(String args []){
+  public static void main(String args []){
  String identitas = "Chatlea Cinta Putri Widyanto/ X RPL 5/ 08";
  tampilJudul(identitas);
-  int n = tampilInput();
-  BigInteger hasil = fibo(n);
-  tampilHasil(n, hasil);
 }
  private static void tampilJudul(String identitas){
      System.out.println("Identitas:" + identitas);
      
      System.out.println("\nHitung Fibonacci");
      System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
- }
-
- private static int tampilInput(){
-     Scanner scanner = new Scanner(System.in);
-     
-     System.out.print("Bilangan ke-: ");
-     int n = scanner.nextInt();
-     
-     return n;   
- }
- 
- private static BigInteger fibo (int n){
-     
-     BigInteger[] hasil = new BigInteger[n];
-     
-     hasil[0] = BigInteger.ONE;
-     hasil[1] = BigInteger.ONE;
-     
-     for (int i = 2; i<n; i++){
-         hasil[i] = hasil[i-1].add(hasil[i-2]);
-     }
-     return hasil[n-1];
- }
- 
- private static void tampilHasil(int n, BigInteger hasil) {
-     System.out.println("Bilangan Fibonacci ke-" +n+ " : " +hasil);
- 
-
  }
 }
 
