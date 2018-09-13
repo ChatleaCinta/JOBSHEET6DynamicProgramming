2b77daf2df77cf114b08d28712a59f06acdce38b
PC <PC@LAPTOP-HTFI5LEL>
2018-09-13 10:17:42 +0700

    Tambah Input

--- src/jobsheet6dynamicprogramming/hitungfibonacci.java
+++ src/jobsheet6dynamicprogramming/hitungfibonacci.java
@@ -16,12 +16,21 @@ public class hitungfibonacci {
   public static void main(String args []){
  String identitas = "Chatlea Cinta Putri Widyanto/ X RPL 5/ 08";
  tampilJudul(identitas);
+  int n = tampilInput();
 }
  private static void tampilJudul(String identitas){
      System.out.println("Identitas:" + identitas);
      
      System.out.println("\nHitung Fibonacci");
      System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+ }
+ private static int tampilInput(){
+     Scanner scanner = new Scanner(System.in);
+     
+     System.out.print("Bilangan ke-: ");
+     int n = scanner.nextInt();
+     
+     return n;   
  }
 }
 
