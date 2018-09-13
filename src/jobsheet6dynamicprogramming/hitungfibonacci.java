23e5a957514ed9cca4742c03573634cb296c373a
PC <PC@LAPTOP-HTFI5LEL>
2018-09-13 10:20:37 +0700

    Tambah Tampil Hasil

--- src/jobsheet6dynamicprogramming/hitungfibonacci.java
+++ src/jobsheet6dynamicprogramming/hitungfibonacci.java
@@ -18,6 +18,7 @@ public class hitungfibonacci {
  tampilJudul(identitas);
   int n = tampilInput();
   BigInteger hasil = fibo(n);
+   tampilHasil(n, hasil);
 }
  private static void tampilJudul(String identitas){
      System.out.println("Identitas:" + identitas);
@@ -44,6 +45,11 @@ public class hitungfibonacci {
          hasil[i] = hasil[i-1].add(hasil[i-2]);
      }
      return hasil[n-1];
+ }
+   private static void tampilHasil(int n, BigInteger hasil) {
+     System.out.println("Bilangan Fibonacci ke-" +n+ " : " +hasil);
+ 
+
  }
 }
 
